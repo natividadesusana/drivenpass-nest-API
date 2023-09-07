fd82ff445d7bad3bcd12dab8592f48df5e21d4b4
natividadesusana <susanajdsn@gmail.com>
2023-09-07 20:45:08 -0300

    refactor: app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/health')
+  @ApiOperation({
+    summary: 'Checks APIs health',
+    description: 'Request to the app',
+  })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Everything is okay' })
   getHello(): string {
     return this.appService.getHealth();
   }
