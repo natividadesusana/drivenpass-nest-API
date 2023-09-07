a8842b5f3e1fbb55e039d856740e68fecf3f13c7
natividadesusana <susanajdsn@gmail.com>
2023-09-07 20:45:50 -0300

    refactor: main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('DrivenPass - Rest API')
+    .setDescription('DrivenPass API description')
+    .setVersion('1.0')
+    .addTag('drivenpass')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+      },
+      'Authorization',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     console.log('Server is up and running on port: ' + port);
