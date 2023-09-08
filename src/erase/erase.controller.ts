e3efe4fa632fc07f3ee476ec081e055dd8970f9d
natividadesusana <susanajdsn@gmail.com>
2023-09-07 21:08:54 -0300

    refactor: erase

--- src/erase/erase.controller.ts
+++ src/erase/erase.controller.ts
@@ -2,16 +2,11 @@ import { Body, Controller, Delete, UseGuards } from '@nestjs/common';
 import { EraseService } from './erase.service';
 import { Users } from '@/decorators/user.decorator';
 import { User } from '@prisma/client';
-import { EraseDTO } from './dto/erase.dto';
+import { EraseDto } from './dto/erase.dto';
 import { AuthGuard } from '@/guards/auth.guard';
-import {
-  ApiBearerAuth,
-  ApiBody,
-  ApiOkResponse,
-  ApiOperation,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
+@ApiTags('Erase')
 @ApiBearerAuth('Authorization')
 @UseGuards(AuthGuard)
 @Controller('erase')
@@ -19,16 +14,11 @@ export class EraseController {
   constructor(private readonly eraseService: EraseService) {}
 
   @Delete()
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: { password: { type: 'string', example: '@Password123' } },
-    },
-  })
+  @ApiBody({ schema: { type: 'object', properties: { password: { type: 'string', example: '@Password123' } } } })
   @ApiOperation({ summary: 'Delete account' })
   @ApiUnauthorizedResponse({ description: 'Token not sent or invalid' })
   @ApiOkResponse({ description: 'Account deleted successfully' })
-  async eraseAccount(@Body() body: EraseDTO, @Users() user: User) {
+  async eraseAccount(@Body() body: EraseDto, @Users() user: User) {
     await this.eraseService.deleteAccount(user.email, body.password);
     return { message: 'Account successfully deleted' };
   }
