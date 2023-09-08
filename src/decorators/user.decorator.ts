60691429964eae1904b445fdc9286090c57cfc8c
natividadesusana <susanajdsn@gmail.com>
2023-09-07 21:07:31 -0300

    refactor: decorators

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -3,10 +3,7 @@ import { ExecutionContext, NotFoundException, createParamDecorator } from '@nest
 export const Users = createParamDecorator(
   (data: string, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
-    if (!request.user) {
-      throw new NotFoundException('User not found.');
-    }
-
+    if (!request.user) throw new NotFoundException('User not found.');
     return request.user;
   },
 );
